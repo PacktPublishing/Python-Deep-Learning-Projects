651b33636d95f6b1268738706b4f0c1033f8fd97
Abhishek Nagaraja <abhishek.bn93@gmail.com>
2018-09-19 19:47:03 -0500

    Chapter 8

---
+++ Chapter 8/data_aug.py
@@ -0,0 +1,74 @@
+"""This module implements a deep conv classifier with max pool and dropout."""
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, MaxPool2D, Dropout
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+
+from sklearn.model_selection import train_test_split
+from loss_plot import loss_plot
+
+# Number of epochs
+epochs = 10
+# Batchsize
+batch_size = 128
+# Optimizer for the generator
+from keras.optimizers import Adam
+optimizer = Adam(lr=0.001)
+# Shape of the input image
+input_shape = (28,28,1)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                  stratify = y_train,
+                                                  test_size = 0.08333,
+                                                  random_state=42)
+
+X_train = X_train.reshape(-1,28,28,1)
+X_val = X_val.reshape(-1,28,28,1)
+X_test = X_test.reshape(-1,28,28,1)
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+train_datagen.fit(X_train)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3,3), input_shape=input_shape,
+                 activation = 'relu'))
+model.add(MaxPool2D(2,2))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, kernel_size=(3,3), activation = 'relu'))
+model.add(MaxPool2D(2,2))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, kernel_size=(3,3), activation = 'relu'))
+model.add(MaxPool2D(2,2))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10, activation = 'softmax'))
+
+model.compile(loss = 'sparse_categorical_crossentropy', optimizer= optimizer,
+              metrics = ['accuracy'])
+
+# fits the model on batches with real-time data augmentation:
+history = model.fit_generator(train_datagen.flow(X_train, y_train,
+                                                 batch_size=128),
+                              steps_per_epoch=len(X_train) / 128, epochs=epochs,
+                              validation_data=(train_datagen.flow(X_val,
+                                                                  y_val)))
+
+for test_img, test_lab in train_datagen.flow(X_test, y_test,
+                                             batch_size = X_test.shape[0]):
+    break
+
+loss,acc = model.evaluate(test_img, test_lab)
+print('Test loss:', loss)
+print('Accuracy:', acc)
+
+loss_plot(history)
